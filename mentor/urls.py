49ff7ad0e549b2356b01ffb0839c693c5058da82
Pranjal Singh <pranjal366@gmail.com>
2021-09-26 23:16:47 +0530

    Preview Profile

--- mentor/urls.py
+++ mentor/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('follow/<str:pk>', views.follow_request, name='follow_request'),
     path('unfollow/<str:pk>', views.unfollow_request, name='unfollow_request'),
     path('update/', views.accountupdate, name='profile_update'),
+    path('profile/<str:pk>', views.profileview, name='profileview'),
 ]
