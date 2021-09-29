bc3d67245f7effacc5d0f3eff579457b6f426937
Pranjal Singh <pranjal366@gmail.com>
2021-09-29 19:26:19 +0530

    Nexus

--- mentor/models.py
+++ mentor/models.py
@@ -46,3 +46,26 @@ class MenteeInfo(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    creator = models.ForeignKey(MenteeInfo, on_delete=models.CASCADE, related_name='created_by')
+    post_caption = models.CharField(max_length=100, blank=True)
+    post_text = models.CharField(max_length=300, blank=True)
+    post_image = models.ImageField(blank=True)
+    post_video = models.FileField(blank=True)
+    likes = models.ManyToManyField(MenteeInfo, related_name='liked_by', blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.creator.user.username + ' ' + str(self.id)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_related')
+    body = models.CharField(max_length=300, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    commentor = models.ForeignKey(MenteeInfo, related_name='commented_by', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.post.creator.user.username + ' ' + str(self.post.creator.user.id)
--- mentor/urls.py
+++ mentor/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('profile/<str:pk>', views.profileview, name='profileview'),
     path('post/', views.post, name='post'),
     path('post/all', views.post_all, name='post_all'),
+    path('post/new', views.create_post, name='create'),
+    path('post/like', views.post_like, name='like'),
+    path('post/unlike', views.post_unlike, name='unlike'),
+    path('post/comment/<str:pk>', views.post_comment, name='comment'),
 ]
